3b84a261a8f8dbe4cdd9d119f2dcd2981a77ae66
kishormahmud9 <topfazil999@gmail.com>
2024-03-12 15:31:01 +0600

    frontend mastering complete

--- resources/views/frontend/indexContent/testimonial.blade.php
+++ resources/views/frontend/indexContent/testimonial.blade.php
@@ -1,129 +1,125 @@
-<!--=========== Testimonial Section Start =========-->
-<section class="tj-testimonial-section-two">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="thumb-content-area">
-                    <div class="active-image">
-                        <div class="testimonial_active_img active">
-                            <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-slider-1.jpg"
-                                alt="Image" />
-                        </div>
-                        <div class="testimonial_active_img">
-                            <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-slider-2.jpg"
-                                alt="Image" />
-                        </div>
-                        <div class="testimonial_active_img">
-                            <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-slider-3.jpg"
-                                alt="Image" />
-                        </div>
-                    </div>
-                    <div class="swiper thumb-testimonial-slider">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide swiper-slide-active">
-                                <div class="thumb-image">
-                                    <img src="{{ asset('frontend') }}/assets/images/testimonial/slider-image1.png"
-                                        alt="Image" />
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="thumb-image">
-                                    <img src="{{ asset('frontend') }}/assets/images/testimonial/slider-image2.png"
-                                        alt="Image" />
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="thumb-image">
-                                    <img src="{{ asset('frontend') }}/assets/images/testimonial/slider-image3.png"
-                                        alt="Image" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="testimonial-content-area">
-                    <div class="tj-sec-heading-two">
-                        <span class="sub-title">Testimonials</span>
-                        <h2 class="title">Client’s <span>Reviews</span></h2>
-                    </div>
-                </div>
-                <div class="swiper thumb-slider2">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="testimonial-slider-item">
-                                <div class="desc">
-                                    <p>
-                                        Many desktop publishing packages and web page editors now use Lorem
-                                        Ipsum as their default model text, and a search for 'lorem ipsum'
-                                        will uncover many web sites still in their infancy. Various versions
-                                        have evolved over the years, sometimes by accident, sometimes on
-                                        purpose injected humour and the like.
-                                    </p>
-                                </div>
-                                <div class="testimonial-auother">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('frontend') }}/assets/images/icon/quote.svg"
-                                            alt="Icon" />
-                                    </div>
-                                    <div class="testimonial-text">
-                                        <h3 class="title">Thomas <span>Walkar</span></h3>
-                                        <span class="sub-title">Sustainable energy owner</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="testimonial-slider-item">
-                                <div class="desc">
-                                    <p>
-                                        Many desktop publishing packages and web page editors now use Lorem
-                                        Ipsum as their default model text, and a search for 'lorem ipsum'
-                                        will uncover many web sites still in their infancy. Various versions
-                                        have evolved over the years, sometimes by accident, sometimes on
-                                        purpose injected humour and the like.
-                                    </p>
-                                </div>
-                                <div class="testimonial-auother">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('frontend') }}/assets/images/icon/quote.svg"
-                                            alt="Icon" />
-                                    </div>
-                                    <div class="testimonial-text">
-                                        <h3 class="title">Thomas <span>Walkar</span></h3>
-                                        <span class="sub-title">Sustainable energy owner</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="testimonial-slider-item">
-                                <div class="desc">
-                                    <p>
-                                        Many desktop publishing packages and web page editors now use Lorem
-                                        Ipsum as their default model text, and a search for 'lorem ipsum'
-                                        will uncover many web sites still in their infancy. Various versions
-                                        have evolved over the years, sometimes by accident, sometimes on
-                                        purpose injected humour and the like.
-                                    </p>
-                                </div>
-                                <div class="testimonial-auother">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('frontend') }}/assets/images/icon/quote.svg"
-                                            alt="Icon" />
-                                    </div>
-                                    <div class="testimonial-text">
-                                        <h3 class="title">Thomas <span>Walkar</span></h3>
-                                        <span class="sub-title">Sustainable energy owner</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!--=========== Testimonial Section End =========-->
+ <!--=========== Testimonial Section Start =========-->
+ <section class="tj-testimonial-section">
+     <div class="container">
+         <div class="row align-items-center">
+             <div class="col-lg-7">
+                 <div class="tj-sec-heading">
+                     <span class="sub-title">Testimonial</span>
+                     <h2 class="sec-title">
+                         Check Our Clients <br />
+                         <span>Feedback</span>
+                     </h2>
+                 </div>
+                 <div class="swiper tj-testimonial-slider">
+                     <div class="swiper-wrapper">
+                         <div class="swiper-slide">
+                             <div class="tj-testimonial-item">
+                                 <div class="desc">
+                                     <p>
+                                         ‘’Lorem Ipsum is not simply random text. It has roots in a piece of
+                                         classical Latin literature from 45 BC, making it over 2000 years
+                                         old. Richard McClintock, a Latin professor at Hampden’’
+                                     </p>
+                                 </div>
+                                 <div class="testimonial-auother">
+                                     <div class="auother-image">
+                                         <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-small-1.png"
+                                             alt="Image" />
+                                     </div>
+                                     <div class="auother-header">
+                                         <h5 class="title">
+                                             Richard <span class="active-text">Thomas</span>
+                                         </h5>
+                                         <span class="sub-title">Manager</span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="swiper-slide">
+                             <div class="tj-testimonial-item">
+                                 <div class="desc">
+                                     <p>
+                                         ‘’Lorem Ipsum is not simply random text. It has roots in a piece of
+                                         classical Latin literature from 45 BC, making it over 2000 years
+                                         old. Richard McClintock, a Latin professor at Hampden’’
+                                     </p>
+                                 </div>
+                                 <div class="testimonial-auother">
+                                     <div class="auother-image">
+                                         <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-small-2.png"
+                                             alt="Image" />
+                                     </div>
+                                     <div class="auother-header">
+                                         <h5 class="title">
+                                             Richard <span class="active-text">Thomas</span>
+                                         </h5>
+                                         <span class="sub-title">Manager</span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                         <div class="swiper-slide">
+                             <div class="tj-testimonial-item">
+                                 <div class="desc">
+                                     <p>
+                                         ‘’Lorem Ipsum is not simply random text. It has roots in a piece of
+                                         classical Latin literature from 45 BC, making it over 2000 years
+                                         old. Richard McClintock, a Latin professor at Hampden’’
+                                     </p>
+                                 </div>
+                                 <div class="testimonial-auother">
+                                     <div class="auother-image">
+                                         <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-small-3.png"
+                                             alt="Image" />
+                                     </div>
+                                     <div class="auother-header">
+                                         <h5 class="title">
+                                             Richard <span class="active-text">Thomas</span>
+                                         </h5>
+                                         <span class="sub-title">Manager</span>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="testimonial-navigation">
+                         <div class="testimonial-prev"><i class="flaticon-right-arrow"></i></div>
+                         <div class="testimonial-next">
+                             <i class="flaticon-right-arrow"></i>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-5">
+                 <div class="testimonial-wrapper">
+                     <div class="swiper tj-testimonial-2">
+                         <div class="swiper-wrapper">
+                             <div class="swiper-slide">
+                                 <div class="testimonial-image">
+                                     <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-big-1.jpg"
+                                         alt="Image" />
+                                 </div>
+                             </div>
+                             <div class="swiper-slide">
+                                 <div class="testimonial-image">
+                                     <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-big-2.jpg"
+                                         alt="Image" />
+                                 </div>
+                             </div>
+                             <div class="swiper-slide">
+                                 <div class="testimonial-image">
+                                     <img src="{{ asset('frontend') }}/assets/images/testimonial/testi-big-3.jpg"
+                                         alt="Image" />
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="quote-icon">
+                         <img src="{{ asset('frontend') }}/assets/images/icon/test-quote.svg" alt="Icon" />
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+ <!--=========== Testimonial Section End =========-->
